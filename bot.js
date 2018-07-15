912167ed14d78bf4bad043606307efa9192498b6
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-07-15 13:43:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,6 +85,27 @@ client.on("guildMemberAdd", member => {
 
 
 
+const moment = require('moment');
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","s7q");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:norelden});          
+               
+ 
+      }
+      });
+
+
 
 client.on('ready', () => {
    console.log(`----------------`);
