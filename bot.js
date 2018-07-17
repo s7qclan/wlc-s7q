d3f26189f2fac9fd09e28909c37628affee3057d
s7qclan <41242298+s7qclan@users.noreply.github.com>
2018-07-17 14:29:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,6 +106,50 @@ let welcomer = member.guild.channels.find("name","s7q");
       });
 
 
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("442919008715669505")// ايدي السيرفر
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("466346716984115250");// ايدي الروم
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("442919008715669505")// ايدي السيرفر
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته من قبل  ${Invite.inviter}`)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+
+
 
 client.on('ready', () => {
    console.log(`----------------`);
